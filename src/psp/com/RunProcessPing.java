f616cf283dcfb7873c9f57ab084e24559c7e39fc
Alejandro Mechine Gamarra <alejandromechinegamarra@gmail.com>
2017-10-06 11:58:49 +0200

    TErminadito, faltan commits

--- src/psp/com/RunProcessPing.java
+++ src/psp/com/RunProcessPing.java
@@ -1,50 +1,31 @@
 package psp.com;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class RunProcessPing {
 	public static void main(String[] args) {
-			String comando = "/bin/ping www.google.com";
+			Process proceso = null;
 			
-			ProcessBuilder pb = new ProcessBuilder(args);
-			pb.redirectErrorStream(true);
+		
 			
 			try {
-				Process proceso = pb.start();
-				MostrarSalidaProceso(proceso);			
-				System.exit(0);
+				proceso = new ProcessBuilder("ping","www.google.com").start();
+				BufferedReader in = new BufferedReader(
+					new InputStreamReader(proceso.getInputStream(), "utf-8"));
+				for (int i = 0; i < 5; i++) {
+					System.out.println(in.readLine());
+				}
+				if (proceso != null) {
+					proceso.destroy();
+				}		
 				
-			} catch (Exception e) {
+				
+				
+			} catch (IOException e) {
 				System.err.println("Error de E/S");
 				System.exit(-1);
 			}
 		}
-
-	private static void MostrarSalidaProceso(Process proceso) {
-		int nLineas = 0;
-		try {
-			proceso.wait(5000);
-			int retorno = proceso.waitFor();;
-			
-			System.out.println("El proceso hijo ha devuelto: "+ retorno);
-			
-			InputStreamReader lector = new InputStreamReader(proceso.getInputStream(),"utf-8");
-			
-			BufferedReader br = new BufferedReader(lector);
-
-			String linea;		
-						
-			while (((linea = br.readLine()) != null) && nLineas <= 5) {
-				
-				System.out.println(linea);
-				nLineas++;				
-			}		
-			
-		}		
-		catch (Exception e) {
-			e.printStackTrace();
-		}
-	
-	}
 }
